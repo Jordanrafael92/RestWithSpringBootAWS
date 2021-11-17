02a272549ff9f3e75ee5d49d0ef12d052b81099b
Jordan <jordanrafael92@gmail.com>
2021-11-17 19:19:01 -0300

    Created Project

---
+++ src/main/java/br/com/jordansilva/restwithspringbootaws/RestWithSpringBootAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.jordansilva.restwithspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jordansilva/restwithspringbootaws/RestWithSpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jordansilva.restwithspringbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
