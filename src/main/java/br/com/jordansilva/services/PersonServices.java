711812634976d47f2e14e007fb0f1254f93e32ed
Jordan <jordanrafael92@gmail.com>
2021-11-18 16:49:54 -0300

    Return list of person

--- src/main/java/br/com/jordansilva/services/PersonServices.java
+++ src/main/java/br/com/jordansilva/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.jordansilva.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -17,6 +19,25 @@ public class PersonServices {
 		person.setFirstName("Jordan");
 		person.setLastName("Silva");
 		person.setAddress("Av América - PR");
+		person.setGender("Male");
+		return person ;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person Name " + i);
+		person.setLastName("Last name "  + i);
+		person.setAddress("Some address in Brazil " + i);
 		person.setGender("Male");
 		return person ;
 	}
